aa29cfecfaa6e5a7b0e03fe59180f1d2a5530464
seohee <victory8870@naver.com>
2024-05-31 03:18:00 +0900

    타이머
    타이머 페이지 제외하고
    초기 설정 추가 및 수정

--- front/running_mate/lib/main.dart
+++ front/running_mate/lib/main.dart
@@ -3,27 +3,28 @@ import 'package:running_mate/screens/LoginScreen.dart';
 import 'package:running_mate/screens/SplashScreen.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:intl/date_symbol_data_local.dart';
+import 'package:flutter_naver_map/flutter_naver_map.dart';
+import 'package:running_mate/screens/TimerPage.dart';
 
-//
-// 앱의 시작점
-// runApp 함수의 괄호 안에는 위젯이 들어간다.
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-
-  await initializeDateFormatting();
-  // var box = await Hive.openBox('dataBox');
-  runApp(MyApp());
-}
-
-// stl 입력
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: '러닝 메이트',
-      // home: SplashScreen(),
-      home: SplashScreen(),
+      home: SplashScreen(), // 초기 화면을
     );
   }
+}
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
+  await NaverMapSdk.instance.initialize(
+    clientId: '019aslid8u',
+    onAuthFailed: (error) => print('네이버 맵 인증 실패: $error'),
+  );
+
+  await initializeDateFormatting();
+  runApp(MyApp());
 }
